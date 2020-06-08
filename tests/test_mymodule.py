159412964df980c3a0f49cde8e38b3754db1744c
Georgia Kokkinou <geor5ko@gmail.com>
2020-06-08 14:20:26 +0300

    Add docstrings to tests

--- tests/test_mymodule.py
+++ tests/test_mymodule.py
@@ -1,7 +1,9 @@
 import mymodule
 
 def test_increment():
+    """Test increment."""
     assert mymodule.increment(4) == 5
 
 def test_decrement():
+    """Test decrement."""
     assert mymodule.decrement(4) == 3
